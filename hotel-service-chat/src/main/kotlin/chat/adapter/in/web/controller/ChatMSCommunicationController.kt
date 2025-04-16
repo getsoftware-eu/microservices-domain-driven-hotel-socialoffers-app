f210a4cefbde98383cce7cec89a3a7828e84f93f
Eugen Fanshil <eugen@getsoftware.eu>
2025-04-17 00:26:32 +0200

    fix lombok

--- hotel-service-chat/src/main/kotlin/chat/adapter/in/web/controller/ChatMSCommunicationController.kt
+++ hotel-service-chat/src/main/kotlin/chat/adapter/in/web/controller/ChatMSCommunicationController.kt
@@ -1,5 +1,8 @@
 package chat.adapter.`in`.web.controller
 
+//import eu.getsoftware.hotelico.clients.openapi.generated.chat.ChatApi
+//import eu.getsoftware.hotelico.clients.openapi.generated.chat.dto.ChatMsgRestDTO
+//import eu.getsoftware.hotelico.clients.openapi.generated.chat.dto.CustomerRestDTO
 import chat.adapter.out.persistence.model.ChatMessageMappedEntity
 import chat.adapter.out.persistence.model.ChatUserMappedEntity
 import chat.adapter.out.persistence.repository.ChatMessageRepository
@@ -7,31 +10,31 @@ import chat.adapter.out.persistence.repository.ChatUserRepository
 import chat.application.port.out.ChatDTOService
 import eu.getsoftware.hotelico.clients.api.clients.dto.entity.CustomerDTO
 import eu.getsoftware.hotelico.clients.api.clients.infrastructure.chat.dto.ChatMsgDTO
-import eu.getsoftware.hotelico.clients.openapi.generated.chat.ChatApi
-import eu.getsoftware.hotelico.clients.openapi.generated.chat.dto.ChatMsgRestDTO
-import eu.getsoftware.hotelico.clients.openapi.generated.chat.dto.CustomerRestDTO
 import lombok.RequiredArgsConstructor
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@Controller
 @RequestMapping("/api/v0/chat")
 @RequiredArgsConstructor
 class ChatMSCommunicationController(
     private val chatMessageRepository: ChatMessageRepository,
     private val chatUserRepository: ChatUserRepository,
     private val chatDTOService: ChatDTOService
-) : ChatApi {
+) //: ChatApi 
+{
 
     //	@PostMapping("/message")  in interface
-    override fun postMessage(@RequestBody msgDTO: ChatMsgRestDTO): ChatMsgDTO {
+    //override 
+    fun postMessage(@RequestBody msgDTO: ChatMsgDTO): ChatMsgDTO {
         val createdEntity: ChatMessageMappedEntity = chatDTOService.createChatMessageFromDTO(msgDTO)
         return chatDTOService.convertToDTO(createdEntity)
     }
 
     //	@PostMapping("/customer") in interface
-    override fun updateUser(@RequestBody customerDTO: CustomerRestDTO): CustomerDTO {
+    //override 
+    fun updateUser(@RequestBody customerDTO: CustomerDTO): CustomerDTO {
         val updateEntity: ChatUserMappedEntity = chatDTOService.updateCustomerFromDTO(customerDTO)
         return chatDTOService.convertToDTO(updateEntity)
     }
